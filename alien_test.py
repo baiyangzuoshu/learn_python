f1ce759648891776c3c82bd7e461c93ff9c76630
maomao <maomao@localhost.localdomain>
2018-10-27 08:48:32 +0800

    字典的创建遍历，items(),keys(),values()

---
+++ alien_test.py
@@ -0,0 +1,7 @@
+alien_0={'name':"maomao","age":25,"sex":"man"}
+for key,value in alien_0.items():
+    print(key,value)
+for key in alien_0.keys():
+    print(key)
+for value in alien_0.values():
+    print(value)
