99c08bd9113107f1dc80987af2b84fb9e998c814
baiyangzuoshu <942357585@qq.com>
2018-11-14 21:31:14 +0800

    消灭外星人

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -16,13 +16,14 @@ def run_game():
     bullets=Group()
     aliens=Group()
 
-    gf.create_fleet(ai_settings,screen,aliens)
+    gf.create_fleet(ai_settings,screen,ship,aliens)
 
     while True:
     	
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
-        gf.bullet_update(bullets)
+        gf.bullet_update(aliens,bullets)
+        gf.update_aliens(ai_settings,aliens)
 
         gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 #run_game()
